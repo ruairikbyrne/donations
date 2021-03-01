e145157cf35731401526600fffc83c9975a6ed28
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-03-01 19:50:06 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const Cookie = require("@hapi/cookie");
+const Joi = require("@hapi/joi");
+const Handlebars = require("handlebars");
+const dotenv = require("dotenv");
+
+//env.config();
+
+const server = Hapi.server({
+  port: process.env.PORT || 3000,
+});
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
+
+async function init() {
+  await server.register(Inert);
+  await server.register(Vision);
+  await server.register(Cookie);
+  server.validator(require("@hapi/joi"));
+  server.views({
+    engines: {
+      hbs: Handlebars,
+    },
+    relativeTo: __dirname,
+    path: "./app/views",
+    layoutPath: "./app/views/layouts",
+    partialsPath: "./app/views/partials",
+    layout: true,
+    isCached: false,
+  });
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false,
+    },
+    redirectTo: "/",
+  });
+
+  server.auth.default("session");
+
+  server.route(require("./routes"));
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+require("./app/models/db");
+
+init();
---
+++ routes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Accounts = require("./app/controllers/accounts");
+const Donations = require("./app/controllers/donations");
+
+module.exports = [
+  { method: "GET", path: "/", config: Accounts.index },
+  { method: "GET", path: "/signup", config: Accounts.showSignup },
+  { method: "GET", path: "/login", config: Accounts.showLogin },
+  { method: "GET", path: "/settings", config: Accounts.showSettings },
+  { method: "GET", path: "/logout", config: Accounts.logout },
+  { method: "POST", path: "/settings", config: Accounts.updateSettings },
+  { method: "POST", path: "/signup", config: Accounts.signup },
+  { method: "POST", path: "/login", config: Accounts.login },
+
+  { method: "GET", path: "/home", config: Donations.home },
+  { method: "GET", path: "/report", config: Donations.report },
+  { method: "POST", path: "/donate", config: Donations.donate },
+
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: "./public",
+      },
+    },
+    options: { auth: false },
+  },
+];
