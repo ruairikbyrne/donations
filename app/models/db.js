4c51d85f25ae3e72375cbffcc00be0afb07e96e1
Ruairi Byrne <ruairikbyrne@hotmail.com>
2021-03-08 18:06:56 +0000

    lab 06a

--- app/models/db.js
+++ app/models/db.js
@@ -11,6 +11,16 @@ Mongoose.set("useUnifiedTopology", true);
 Mongoose.connect(process.env.db);
 const db = Mongoose.connection;
 
+async function seed() {
+  var seeder = require("mais-mongoose-seeder")(Mongoose);
+  const data = require("./seed-data.json");
+  const Donation = require("./donations");
+  const Candidate = require("./candidate.js");
+  const User = require("./user");
+  const dbData = await seeder.seed(data, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
+
 db.on("error", function (err) {
   console.log(`database connection error: ${err}`);
 });
@@ -21,4 +31,5 @@ db.on("disconnected", function () {
 
 db.once("open", function () {
   console.log(`database connected to ${this.name} on ${this.host}`);
+  seed();
 });
